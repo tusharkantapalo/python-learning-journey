a0e2bf4836caf305df4a495c7bf35c8a5b498d21
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-26 09:25:20 +0530

    print with value

--- 26 May 2026.py
+++ 26 May 2026.py
@@ -3,6 +3,6 @@ print(type(x))
 print(id(x))
 print(x)
 y=input("Enter y: ")
-print(y) #this will print the address in C but not in Python
+print("The vlue is: ",y) #to print something with a value write like this
 print("Hello \n world") #new line character
 print("I am Tushar")
