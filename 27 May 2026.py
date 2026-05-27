de250d0af258f0a6a83dd5842008f7a81745f953
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-05-27 09:09:51 +0530

    + using string and int

--- 27 May 2026.py
+++ 27 May 2026.py
@@ -3,3 +3,10 @@ age=input("Enter your age: ")
 print(type(age)) #Here it is a string
 age=int(input("Enter your age: "))
 print(type(age)) #Here it is an integer
+#OPERATORS
+x=input("Enter first number: ")
+y=input("Enter second number: ")
+print(x+y) #we will get the output as 2020, as these are strings
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+print(x+y) #we will get the value as 40
