694182dab0df895d2d310d1679abd4a52f97e228
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-02 20:22:24 +0530

    corrected

--- 2June2026/lab/flat.py
+++ 2June2026/lab/flat.py
@@ -1,15 +1,10 @@
 def flatten(lst):
     result = []
-
     for item in lst:
         if type(item) == list:
             result.extend(flatten(item))
         else:
             result.append(item)
-
     return result
-
-
 nested = [1, [2, 3], [4, [5, 6]], 7]
-
 print("Flattened:", flatten(nested))
