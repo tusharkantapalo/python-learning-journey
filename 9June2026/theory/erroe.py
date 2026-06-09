7253c9f7076714b5e35063251b4ecd43a3310b19
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-09 08:42:39 +0530

    implemented try and except

--- 9June2026/theory/erroe.py
+++ 9June2026/theory/erroe.py
@@ -1,6 +1,15 @@
+def divide_two_nos(a: int, b: int) -> float:
+    try:
+        return(a / b)
+    except: #it will except the error if happened
+        pass
+
+
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of a: "))
-if b == 0:
+'''if b == 0:
     print("Division not possible.")
 else:
-    print(a / b)
+    print(a / b)'''
+res = divide_two_nos(a, b)
+print(res)
