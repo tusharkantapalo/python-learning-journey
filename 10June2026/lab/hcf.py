a8d122e7bbf1c26bff4e822248c70fa57c62af78
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-11 07:52:56 +0530

    added finally: to each code

--- 10June2026/lab/hcf.py
+++ 10June2026/lab/hcf.py
@@ -1,9 +1,15 @@
-def hcf(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+try:
+    def hcf(a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-print("HCF = ", hcf(num1, num2))
+    print("HCF = ", hcf(num1, num2))
+except Exception:
+    print("Something wrong happened!")
+
+finally:
+    print("__________CODE EXECUTION COMPLETED__________")
--- 10June2026/lab/no_of_vowel.py
+++ 10June2026/lab/no_of_vowel.py
@@ -1,15 +1,21 @@
-string = input("Enter the string: ")
+try:
+    string = input("Enter the string: ")
 
-vowel_no = {}
+    vowel_no = {}
 
-vowel_l = "aeiou"
-vowel_u = "AEIOU"
+    vowel_l = "aeiou"
+    vowel_u = "AEIOU"
 
-for i in string:
-    if i in vowel_l or i in vowel_u:
-        if i in vowel_no:
-            vowel_no[i] += 1
-        else:
-            vowel_no[i] = 1
+    for i in string:
+        if i in vowel_l or i in vowel_u:
+            if i in vowel_no:
+                vowel_no[i] += 1
+            else:
+                vowel_no[i] = 1
 
-print(vowel_no)
+    print(vowel_no)
+except Exception:
+    print("Something wrong happend!")
+
+finally:
+    print("__________CODE EXECUTION COMPLETED__________")
