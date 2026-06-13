e575cbcec0fba34b47a6c537f1ab5efd0c6e52f1
Tusharkanta Palo <tusharkantapalo@gmail.com>
2026-06-13 10:10:11 +0530

    polymorphism

---
+++ 13June2026/theory/polymorphism.py
@@ -0,0 +1,17 @@
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
